ae41a12edfc8751a5f39db5ce3078a012621f645
Shubhamj08 <sj951563@gmail.com>
2020-06-11 15:29:43 +0530

    button_update_1

---
+++ jScript.js
@@ -0,0 +1,31 @@
+
+function myFunc(id){
+
+  if (id == "kotlin"){
+    window.open("https://kotlinlang.org/", "_blank")
+  }
+
+  if (id == "python"){
+    window.open("https://www.python.org/", "_blank")
+  }
+
+  if (id == "C++"){
+    window.open("https://isocpp.org/", "_blank")
+  }
+
+  if (id == "C"){
+    window.open("https://www.cprogramming.com/", "_blank")
+  }
+
+  if (id == "html"){
+    window.open("https://html.com/", "_blank")
+  }
+
+  if (id == "CSS"){
+    window.open("https://www.w3.org/Style/CSS/Overview.en.html", "_blank")
+  }
+
+  if (id == "bootstrap"){
+    window.open("https://getbootstrap.com/", "_blank")
+  }
+}
